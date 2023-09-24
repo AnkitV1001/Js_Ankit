65638f9d0ca666f2d7a1cfdff3b3528276aa2f74
AnkitV1001 <109146320+AnkitV1001@users.noreply.github.com>
2023-09-24 08:11:36 +0000

    Datatypes done

---
+++ Basics_01/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+3) //we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Ankit")
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object 
+
+console.log(typeof "ankit"); // String
+console.log(typeof null); // object
