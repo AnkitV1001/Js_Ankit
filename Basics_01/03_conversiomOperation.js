fcfb03fc42287ca4d8ffb621911ac05d808a2d21
AnkitV1001 <109146320+AnkitV1001@users.noreply.github.com>
2023-09-24 08:42:49 +0000

    done

---
+++ Basics_01/03_conversiomOperation.js
@@ -0,0 +1,19 @@
+// let score="33abc"
+// let score=null
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);//number
+
+// console.log(valueInNumber);//NaN
+// console.log(valueInNumber);// 0
+
+let isLoggedIn=1
+
+let convertInBoolean=Boolean(isLoggedIn)
+console.log(convertInBoolean);
+
+//1: true, 0: false
+// "" => false
+// "Ankit" => true
