e97bf9f2cb71d2b147490e2635f12b1a6a8dafdc
AnkitV1001 <109146320+AnkitV1001@users.noreply.github.com>
2023-09-26 09:19:54 +0000

    math and num added

---
+++ Basics_01/06_nums_and_math.js
@@ -0,0 +1,46 @@
+const balance =new Number(300)
+
+// console.log(balance); //[Number: 300]
+
+// console.log(balance.toString()); // 300
+
+// console.log(balance.toFixed(3)); //300.000
+
+const otherNumber=23.8955
+
+// console.log(otherNumber.toPrecision(2)); //24
+// console.log(otherNumber.toPrecision(3)); //23.9
+// console.log(otherNumber.toPrecision(1)); //2e+1
+
+
+const hundreds=1000000 // 1,000,000
+
+// console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+
+
+console.log(Math) // Object [Math] {}
+
+console.log(Math.abs(-123)) //123
+
+console.log(Math.round(4.8)) //5
+
+console.log(Math.ceil(4.3)) //5
+
+console.log(Math.floor(4.8)) //4
+
+console.log(Math.min(4,23,1,2,33,4)) //1
+
+console.log(Math.max(4,23,1,2,33,4)) //33
+
+const min=10
+const max=20
+
+
+
+console.log(Math.random()); // 0 to 1
+
+console.log(Math.random()*10); // 0 to 10
+
+
+console.log(Math.floor(Math.random()*(max-min +1)+min))
+
